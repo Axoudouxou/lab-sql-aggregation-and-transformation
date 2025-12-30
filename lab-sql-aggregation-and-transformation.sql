755c4445c32127e697e76fb0474c2f8b47ad4b7e
Axelle <axmbesso.am@gmail.com>
2025-12-30 22:02:42 +0100

    Solved lab

---
+++ lab-sql-aggregation-and-transformation.sql
@@ -0,0 +1,80 @@
+USE sakila;
+SHOW TABLES;
+
+SELECT 
+    FLOOR(AVG(length) / 60) AS hours,
+    FLOOR(AVG(length) % 60) AS minutes
+FROM film;
+
+SELECT 
+    FLOOR(AVG(length) / 60) AS hours,
+    FLOOR(AVG(length) % 60) AS minutes
+FROM film;
+
+SELECT 
+    DATEDIFF(MAX(rental_date), MIN(rental_date)) AS days_operating
+FROM rental;
+
+SELECT 
+    *,
+    MONTH(rental_date) AS rental_month,
+    DAYNAME(rental_date) AS rental_weekday
+FROM rental
+LIMIT 20;
+
+SELECT 
+    *,
+    CASE 
+        WHEN DAYOFWEEK(rental_date) IN (1, 7) THEN 'weekend'
+        ELSE 'workday'
+    END AS DAY_TYPE
+FROM rental
+LIMIT 20;
+
+SELECT 
+    title,
+    IFNULL(rental_duration, 'Not Available') AS rental_duration
+FROM film
+ORDER BY title ASC;
+
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    SUBSTRING(email, 1, 3) AS email_prefix
+FROM customer
+ORDER BY last_name ASC;
+
+SELECT COUNT(*) AS total_films
+FROM film;
+
+SELECT 
+    rating,
+    COUNT(*) AS number_of_films
+FROM film
+GROUP BY rating;
+
+SELECT 
+    rating,
+    COUNT(*) AS number_of_films
+FROM film
+GROUP BY rating
+ORDER BY number_of_films DESC;
+
+SELECT 
+    rating,
+    ROUND(AVG(length), 2) AS mean_duration
+FROM film
+GROUP BY rating
+ORDER BY mean_duration DESC;
+
+SELECT 
+    rating,
+    ROUND(AVG(length), 2) AS mean_duration
+FROM film
+GROUP BY rating
+HAVING AVG(length) > 120
+ORDER BY mean_duration DESC;
+
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
